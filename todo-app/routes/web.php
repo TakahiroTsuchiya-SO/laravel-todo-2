dd0b2076680c61d9135d8416764ead6e9f68bdd8
takahiro <takahiro.tsuchiya@sold-out.co.jp>
2020-07-18 21:28:20 +0900

    end of chapter9

--- todo-app/routes/web.php
+++ todo-app/routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+Route::group(['middleware' => 'auth'], function(){
+
+Route::get('/', 'HomeController@index')->name('home');
+
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
 
 Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
@@ -10,6 +14,6 @@ Route::post('/folders/{id}/tasks/create', 'TaskController@create');
 
 Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
 Route::post('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit');
+});
 
-Route::get('/', 'HomeController@index')->name('home');
 Auth::routes();
