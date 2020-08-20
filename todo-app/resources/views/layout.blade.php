74c71463398c3d960b2e75d9f0d0d6ba74aff2f7
takahiro <takahiro.tsuchiya@sold-out.co.jp>
2020-08-20 15:30:08 +0900

    CSS読み込まれない問題の修正、松岡さんのコメントを受けての修正

--- todo-app/resources/views/layout.blade.php
+++ todo-app/resources/views/layout.blade.php
@@ -7,61 +7,12 @@
   <title>ToDo App</title>
   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/bootflat/2.0.4/css/bootflat.min.css" rel="stylesheet">
-  <!-- styles -->
-  <style>
-    body {
-      background-color: #f4f7f8;
-    }
-
-    .navbar {
-      margin: 2rem 0 2.5rem 0;
-    }
-
-    .my-navbar {
-      align-items: center;
-      background: #333;
-      display: flex;
-      height: 6rem;
-      justify-content: space-between;
-      padding: 0 2%;
-      margin-bottom: 3rem;
-    }
-
-    .my-navbar-brand {
-      font-size: 18px;
-    }
-
-    .my-navbar-brand,
-    .my-navbar-item {
-      color: #8c8c8c;
-    }
-
-    .my-navbar-brand:hover,
-    a.my-navbar-item:hover {
-      color: #ffffff;
-    }
-
-    .table td:nth-child(2),
-    .table td:nth-child(3),
-    .table td:nth-child(4) {
-      white-space: nowrap;
-      width: 1px;
-    }
-
-    .form-control[disabled],
-    .form-control[readonly] {
-      background-color: #fff;
-    }
-  </style>
-  <!--
-  styles.cssをうまく読み込めない
    @yield('styles')
     @if(app('env') == 'production')
       <link href="{{ secure_asset('css/styles.css') }}" rel="stylesheet">
     @else
         <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
     @endif
-  -->
 </head>
 <body>
 <header>
